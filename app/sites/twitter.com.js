78dca1fedf78f13c489df68ab715cb3d487338cc
Shawn Liu <shawn.liu@dotter.me>
2014-12-04 22:40:32 +0800

    add code for twitter.com

--- app/sites/twitter.com.js
+++ app/sites/twitter.com.js
@@ -2,6 +2,7 @@
  * Created by Shawn Liu on 2014/12/3.
  */
 var tools = require("../util/tools");
+var site = "twitter.com";
 exports.registration = function (casper, data, result, timeout, callback) {
 
 }
@@ -14,7 +15,48 @@ exports.operation = function (casper, data, result, callback) {
 
 }
 
+function _followByFollowers(casper, result, timeout, callback) {
+    casper.thenClick("#page-container > div.dashboard.dashboard-left.home-exp-tweetbox > div.DashboardProfileCard.module > div > div.DashboardProfileCard-stats > ul > li:nth-child(3) > a > span.DashboardProfileCard-statLabel.u-block");
+    casper.then(function () {
+        casper.waitUntilVisible(".GridTimeline-items", function () {
+            var total = casper.fetchText("#page-container > div.ProfileCanopy.ProfileCanopy--withNav.ProfileCanopy--large > div > div.ProfileCanopy-navBar > div.AppContainer > div > div.Grid-cell.u-size2of3.u-lg-size3of4 > div > div > ul > li.ProfileNav-item.ProfileNav-item--followers.is-active > a > span.ProfileNav-value");
+            total = parseInt(total);
+            for (var i = 0; i < total; i++) {
+                
+            }
+
+        }, function () {
+            result.status = false;
+            result.message = "Wait timeout when waiting all follower appear.";
+            tools.getScreenShot(casper, data.site, "followByFollowersFailed");
+            callback(result);
+        }, timeout)
+    })
+}
+
+function _followBySearch(casper, text, result, timeout, callback) {
+    casper.waitUntilVisible("#search-query", function () {
+        this.sendKeys("#search-query", text);
+        casper.thenClick("#global-nav-search > span > button");
+        casper.then(function () {
+            casper.waitUntilVisible("#content-main-heading", function () {
+                tools.getScreenShot(casper, site, "followBySearchSuccess");
+                casper.click("ol.big-avatar-list li:nth-child(1) button.user-actions-follow-button");
+            }, function () {
+                result.status = false;
+                result.message = "wait timeout for selector '#global-nav-search > span > button'";
+                tools.getScreenShot(casper, site, "searchError");
+            }, timeout);
+        });
+    }, function () {
+        result.status = false;
+        result.message = "wait timeout for selector '#search-query'";
+        tools.getScreenShot(casper, "twitter.com", "searchError");
+    }, timeout)
+}
+
 function _login(casper, data, result, timeout, callback) {
+    site = data.site;
     casper.waitUntilVisible("#page-container", function () {
         if (casper.exists("signin-email")) {
             casper.waitUntilVisible("#frontpage-signup-form > button", function () {
